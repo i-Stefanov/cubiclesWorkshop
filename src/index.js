f43f84b8928db12e88c43fc00a682eb0c517b9e5
i-Stefanov <iliqni6e@gmail.com>
2023-10-09 19:19:33 +0300

    handlebars setup,add views

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const hbs = require("express-handlebars");
 const PORT = 3000;
+// handlebars configuration
+app.engine(
+  "hbs",
+  //in the object that is passed to the hbs.engine we can set layoutsDir, partialsDir etc.
+  hbs.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+// Set directory for node to look for the views folder ,because it is not in the root directory
+app.set("views", "src/views");
+// Routes
 app.get("/", (req, res) => {
-  res.send("express works");
+  res.render("index");
 });
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
