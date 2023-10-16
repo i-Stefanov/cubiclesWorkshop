19a1b4b64007a113e658282904c0529039c79229
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-16 12:20:57 +0300

    add basic cube model

---
+++ src/models/Cube.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+// create cube schema (the model of the cube data that will be stored in DB)
+const cubeSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  imageUrl: String,
+  difficultyLevel: Number,
+});
+// set the model name to Cube and use the cubeSchema
+const Cube = mongoose.model("Cube", cubeSchema);
