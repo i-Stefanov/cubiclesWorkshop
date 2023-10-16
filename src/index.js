32fdc6597b1d9f9223a2a09709ca4b2c898a32cb
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-16 11:59:33 +0300

    setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const handlebarsConfigurator = require("./config/handlebarsConfig");
-
-const PORT = 3000;
 // import expressConfig function
 const expressConfig = require("./config/expressConfig");
+const dbConnect = require("./config/dbConfig");
 const routes = require("./routes");
 
-// call expressConfig with app
+const PORT = 3000;
+//! start server while waiting for the database to connect (option)
 expressConfig(app);
 handlebarsConfigurator(app);
+dbConnect()
+  .then(() => console.log("DB connected successfuly"))
+  .catch((err) => console.log("DB error: " + err));
 // Routes
 app.use(routes);
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
 });
+// ! start server only after DB is connected (option) since dbConnect is async function
+// dbConnect()
+//   .then(() => {
+// call expressConfig with app
+//     expressConfig(app);
+//     handlebarsConfigurator(app);
+// Routes
+//     app.use(routes);
+//     app.listen(PORT, () => {
+//       console.log("Server is running on port: " + PORT);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("DB error: " + err);
+//   });
