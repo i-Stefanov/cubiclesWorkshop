bc91a2157ce1a5002ae186eed6322e07ef57ada1
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-18 14:30:03 +0300

    add resources and add createAccessory page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ const router = require("express").Router();
 // homeController = router from homeController.js file
 const homeController = require("./controllers/homeController");
 const cubeController = require("./controllers/cubeController");
+const accessoryController = require("./controllers/accessoryController");
 
 router.use(homeController);
 // use cubeController for all requests that start with /cubes
 router.use("/cubes", cubeController);
+router.use("/accessories", accessoryController);
 // if route does not exist redirect to 404 page
 router.get("*", (req, res) => {
   res.redirect("/404");
 });
+
 module.exports = router;
