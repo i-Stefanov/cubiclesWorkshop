8f07c4ba500c692ced71dc3e53ab69d404b9d185
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-19 11:58:03 +0300

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,8 @@
+const { urlencoded } = require("body-parser");
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
