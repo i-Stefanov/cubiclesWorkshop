395ba5dfabb08eed4a56c103c861bba32562482f
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-10-19 13:26:33 +0300

    hash password before save in DB

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const { urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   username: String,
   password: {
@@ -19,6 +20,12 @@ userSchema.virtual("repeatPassword").set(function (value) {
   if (value !== this.password) {
     throw new mongoose.MongooseError("Passwords don't match!");
   }
+});
+userSchema.pre("save", async function () {
+  // the second paramenter 10 refers to the rounds of salt (cost factor) controls how much time takes to generate the hash and salt that makes brute force less probable
+  const hash = await bcrypt.hash(this.password, 10);
+  //replace teh password with the hash
+  this.password = hash;
 });
 const User = mongoose.model("User", userSchema);
 module.exports = User;
